7f26a64845712e9d8dc8a16bd076e7201d6eaa6d
hzkm613 <hzkm613@gmail.com>
2025-02-15 15:45:10 -0500

    Revert "Update vite.config.js"
    This reverts commit 1a118945bee99bae58419e6e05211bd96cf6486c.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/valentines-day/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-});
+  plugins: [react(), tailwindcss(),],
+  base: "./",
+})
